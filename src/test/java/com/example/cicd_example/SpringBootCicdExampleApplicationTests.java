66134fd4bbdc1ae947a9c7a09008fa2b329927cf
rjramos70 <rjramos70@gmail.com>
2024-07-22 00:35:35 +0100

    Update .gitignore file

--- src/test/java/com/example/cicd_example/SpringBootCicdExampleApplicationTests.java
+++ src/test/java/com/example/cicd_example/SpringBootCicdExampleApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.cicd_example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringBootCicdExampleApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		assertTrue(true);
+
 	}
 
 }
